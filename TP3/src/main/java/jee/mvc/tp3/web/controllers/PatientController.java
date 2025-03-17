58a2e1f193ff4cb35f33fe3711b11fe45640fd15
HakimMohamed <mohamed.hakim.dev@gmail.com>
2025-03-17 13:53:19 +0000

    Activité Pratique N°3 - Show Patients

--- TP3/src/main/java/jee/mvc/tp3/web/controllers/PatientController.java
+++ TP3/src/main/java/jee/mvc/tp3/web/controllers/PatientController.java
@@ -1,9 +1,13 @@
 package jee.mvc.tp3.web.controllers;
 
+import jee.mvc.tp3.entities.Patient;
 import jee.mvc.tp3.services.PatientService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -11,7 +15,9 @@ public class PatientController {
     private PatientService patientService;
 
     @GetMapping("/patients")
-    public String getPatients() {
+    public String getPatients(Model model) {
+        List<Patient> patients = patientService.getPatients();
+        model.addAttribute("patients", patients);
         return "patients/index";
     }
 }
