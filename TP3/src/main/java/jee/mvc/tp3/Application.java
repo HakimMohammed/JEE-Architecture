e6c472fd4c21267ba6f38beb09fbc0aed5a4137b
HakimMohamed <mohamed.hakim.dev@gmail.com>
2025-04-13 12:58:48 +0100

    Activité Pratique N°3 - Partie 3: Identify authenticated user

--- TP3/src/main/java/jee/mvc/tp3/Application.java
+++ TP3/src/main/java/jee/mvc/tp3/Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -31,6 +33,11 @@ public class Application {
                 patientService.addPatient(patient);
             });
         };
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
